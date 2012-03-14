3aa238b65f3a660cd6a53af929ce00046902d3a4
Number1 <pat.orders@gmail.com>
2012-03-13 20:48:26 -0600

    first commit

--- lib/siriproxy-coloradolotto.rb
+++ lib/siriproxy-coloradolotto.rb
@@ -4,7 +4,7 @@ require 'cora'
 require 'siri_objects'
 require 'pp'
 
-class SiriProxy::Plugin::coloradolotto < SiriProxy::Plugin
+class SiriProxy::Plugin::Lotto < SiriProxy::Plugin
     
     def initialize(config = {})
         
